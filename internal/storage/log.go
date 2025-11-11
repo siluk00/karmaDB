2438e56e64d2e73bcc81e11621387def17b7d57c
siluk00 <sil-uk@hotmail.com>
2025-11-10 21:38:42 -0300

    log+

--- internal/storage/log.go
+++ internal/storage/log.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
@@ -8,6 +9,9 @@ import (
 type TransactionLogger interface {
 	WriteDelete(key string)
 	WritePut(key, value string)
+	Err() <-chan error
+	ReadEvents() (<-chan Event, <-chan error)
+	Run()
 }
 
 type Event struct {
@@ -51,4 +55,96 @@ func NewFileTransctionLogger(filename string) (TransactionLogger, error) {
 	}
 
 	return &FileTransactionLogger{file: file}, nil
+}
+
+func (l *FileTransactionLogger) Run() {
+	events := make(chan Event, 16)
+	l.events = events
+
+	errors := make(chan error, 1)
+	l.errors = errors
+
+	go func() {
+		for e := range events {
+			l.lastSequence++
+
+			_, err := fmt.Fprintf(l.file, "%d\t%d\t%s\t%s\t\n", l.lastSequence, e.EventType, e.Key, e.Value)
+
+			if err != nil {
+				errors <- err
+				return
+			}
+		}
+	}()
+}
+
+func (l *FileTransactionLogger) ReadEvents() (<-chan Event, <-chan error) {
+	scanner := bufio.NewScanner(l.file)
+	outEvent := make(chan Event)
+	outError := make(chan error, 1)
+
+	go func() {
+		var e Event
+
+		defer close(outEvent)
+		defer close(outError)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			if _, err := fmt.Sscanf(line, "%d\t%d\t%s\t%s", &e.Sequence, &e.EventType, &e.Key, &e.Value); err != nil {
+				outError <- fmt.Errorf("input parse error: %w", err)
+				return
+			}
+
+			if l.lastSequence >= e.Sequence {
+				outError <- fmt.Errorf("transaction naumbers out of sequence")
+				return
+			}
+
+			l.lastSequence = e.Sequence
+
+			outEvent <- e
+		}
+
+		if err := scanner.Err(); err != nil {
+			outError <- fmt.Errorf("transaction log read failure: %w", err)
+			return
+		}
+	}()
+
+	return outEvent, outError
+}
+
+var logger TransactionLogger
+
+func initializeTransactionLogger() error {
+	var err error
+
+	logger, err = NewFileTransctionLogger("transaction.log")
+	if err != nil {
+		return fmt.Errorf("filed to create event logger: %w", err)
+	}
+
+	events, errors := logger.ReadEvents()
+
+	e := Event{}
+	ok := true
+
+	for ok && err == nil {
+		select {
+		case err, ok = <-errors:
+		case e, ok = <-events:
+			switch e.EventType {
+			case EventDelete:
+				err = Delete(e.Key)
+			case EventPut:
+				err = Put(e.Key, e.Value)
+			}
+		}
+	}
+
+	logger.Run()
+
+	return err
 }
